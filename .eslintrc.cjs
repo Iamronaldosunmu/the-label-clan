e432c3860a3a2923ec3938967498e46e42416d6c
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2023-08-21 21:22:40 +0100

    Feat: Updated eslint config and changed package manager to yarn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2021: true, node: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:react/recommended",
+		"plugin:prettier/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh", "react", "@typescript-eslint", "react-hooks"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"react/react-in-jsx-scope": "off",
+		"prettier/prettier": [
+			"warn",
+			{ endOfLine: "auto", useTabs: true },
+			{ usePrettierrc: true },
+		],
+		"no-restricted-imports": "off",
+		"@typescript-eslint/no-restricted-imports": [
+			"warn",
+			{
+				name: "react-redux",
+				importNames: ["useSelector", "useDispatch"],
+				message:
+					"Use typed hooks `useAppDispatch` and `useAppSelector` instead.",
+			},
+		],
+		"react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+		"react-hooks/exhaustive-deps": "warn",
+	},
+};
