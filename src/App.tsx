e2c8d985a8475c38de9a7e83626ee441d0d52c18
Temi-Dire <temidireowoeye@gmail.com>
2023-08-21 21:22:25 +0100

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Test from './test'
 
 function App() {
   return (
     <>
+        <Router>
+      <>
+        <Routes>
+        <Route path="/" element={<Test/>} />
+        </Routes>
+      </>
+    </Router>
     </>
   )
 }
