400368a1ad5243a0f251b96cdd5868b547f68a24
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-12-23 09:18:25 +0100

    add lenis scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 // import Community from "./Components/Community";
 // import Footer from "./Components/Footer";
-// import { useState } from "react";
+import { useEffect } from "react";
 import HomePage from "./routing/HomePage";
 // import Services from "./Components/Services";
 // import "./App.css";
+import Lenis from "@studio-freight/lenis";
 
 export default function App() {
+	useEffect(() => {
+		const lenis = new Lenis();
+
+		lenis.on("scroll", (e) => {
+			console.log(e);
+		});
+
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	}, []);
 	return (
 		<>
 			<HomePage />
